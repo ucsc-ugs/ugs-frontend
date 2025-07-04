6554f4d0e3023cfa34500c5e986c1d450d1235f2
Ranuga Lecamwasam <ranugalecamwasam2002@gmail.com>
2025-07-04 21:59:17 +0530

    landing page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,5 @@
-// src/pages/Register.tsx
-
-function Register() {
+// src/pages/Notifications.tsx
+function Registrations() {
   return (
     <div className="p-6">
       <h1 className="text-2xl md:text-4xl font-extrabold text-blue-800/50 drop-shadow-sm tracking-wide animate-fadeIn">
@@ -10,4 +9,4 @@ function Register() {
   );
 }
 
-export default Register;
+export default Registrations;
